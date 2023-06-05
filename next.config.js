06b7b54506e2dd3b3e372c104a375e2644ee2c4f
SeylumVA <sc2nem@gmail.com>
2023-06-04 21:48:00 -0400

    Added clerk to allowed image sources

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "res.cloudinary.com",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
     ],
   },
 };
