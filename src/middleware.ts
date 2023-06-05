aed3534a5fc67dfcc2d9d12f5a39b53386addc17
SeylumVA <sc2nem@gmail.com>
2023-06-04 22:12:19 -0400

    Made user webhook route public

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/register"],
+  publicRoutes: ["/", "/api/user"],
 });
 
 export const config = {
